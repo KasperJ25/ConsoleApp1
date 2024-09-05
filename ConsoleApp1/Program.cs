0d34add586d20b39e3dfa3f1fafe54517faaf17e
Kasper Jørgensen <kj@seosoft.dk>
2024-09-05 09:24:53 +0200

    sssssssss

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -25,7 +25,7 @@ namespace ConsoleApp1
             var assembly = Assembly.GetExecutingAssembly();
             var assemblyName = assembly.GetName().Name;
             var gitVersionInformationType = assembly.GetType("GitVersionInformation");
-            //var versionField = gitVersionInformationType.GetField("Major"); AAAAAaAAAA
+            //var versionField = gitVersionInformationType.GetField("Major"); AAAAAaAAAAAAAAA
 
             Console.WriteLine($"Assembly: {assembly.ToString()}");
             Console.WriteLine($"Assembly Name: {assemblyName}");
