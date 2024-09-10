b16ccd68274ac0f7efcf5c0a86e5007881f3bf88
Kasper Jørgensen <kj@seosoft.dk>
2024-09-10 09:27:15 +0200

    This is a Test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,19 +9,21 @@ namespace ConsoleApp1
             string major = Environment.GetEnvironmentVariable("GitVersion_Major");
             string minor = Environment.GetEnvironmentVariable("GitVersion_Minor");
             string patch = Environment.GetEnvironmentVariable("GitVersion_Patch");
-            string semVer = Environment.GetEnvironmentVariable("GitVersion_SemVer"); 
+            string semVer = Environment.GetEnvironmentVariable("GitVersion_SemVer");
             string informationalVersion = Environment.GetEnvironmentVariable("GitVersion_InformationalVersion");
 
             Console.WriteLine($"GitVersion MajorR: {major}");
             Console.WriteLine($"GitVersion Minor: {minor}");
             Console.WriteLine($"GitVersion Patch: {patch}");
             Console.WriteLine($"Full SemVer: {semVer}");
-            Console.WriteLine($"Informational Version: {informationalVersion}"); 
+            Console.WriteLine($"Informational Version: {informationalVersion}");
 
+            HelloWorld();
             GetVersionS();
+            CountToOneHundred();
         }
 
-        public static void GetVersionS() 
+        public static void GetVersionS()
         {
             var assembly = Assembly.GetExecutingAssembly();
             var versionAttribute = (AssemblyInformationalVersionAttribute)assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)[0];
@@ -30,6 +32,20 @@ namespace ConsoleApp1
             Console.WriteLine($"Assembly: {version}");
             //Console.WriteLine($"Major: {versionField}"); A
 
+        }
+
+        public static void HelloWorld()
+        {
+            Console.WriteLine($"Hello World!");
+
+        }
+
+        public static void CountToOneHundred()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
